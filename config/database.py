09ffcf9c4e47f68bc51638615f4fbcb22eb474e6
FelipeLizabello <124591215+FelipeLizabello@users.noreply.github.com>
2026-05-12 21:15:20 -0300

    Add .gitignore file and remove unused database and DnD JSON files

--- config/database.py
+++
@@ -1,9 +0,0 @@
-# from pymongo import MongoClient
-
-# client = MongoClient("mongodb+srv://admin:test1234@cluster0.basabgi.mongodb.net/?appName=Cluster0")
-
-# db = client.race_db
-
-# collection_race = db["race_collection"]
-
-
